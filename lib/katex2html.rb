4c3fb2eb335587ca5588d3270fdd997adcfcebc9
Joao Fraga <jgfraga@gmail.com>
2015-11-26 15:50:42 -0200

    Fix public interface

--- lib/katex2html.rb
+++ lib/katex2html.rb
@@ -5,9 +5,9 @@ require "katex2html/render"
 module Katex2HTML
   HTML_LATEX = "<p>Considere a, b e c números reais maiores que 1, tais que $log_{ba} = 2$ e $log_{bc} = 3$. O valor da expressão:</p><p>$log_c (a.c) + 3log_c b - 2log_c \\left(\\dfrac{a}{b}\\right)$ é:</p>"
 
-  def self.render(to_convert, options)
+  def self.render(to_convert, options = {})
     options = default_opts.merge(options)
-    send(options.renderer, to_convert, options)
+    send(options[:renderer], to_convert, options)
   end
 
   def self.html(html_body, options)
@@ -15,7 +15,7 @@ module Katex2HTML
   end
 
   def self.single(latex, options)
-    Render.new(latex).render
+    Render.new.render(latex)
   end
 
   private
