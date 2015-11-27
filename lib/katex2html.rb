3748bb7d31674581e02bcb8285ac2bee23fbeaf8
Joao Fraga <jgfraga@gmail.com>
2015-11-27 15:21:31 -0200

    Started testing

--- lib/katex2html.rb
+++ lib/katex2html.rb
@@ -5,15 +5,7 @@ require "katex2html/render"
 module Katex2HTML
   def self.render(to_convert, options = {})
     options = default_opts.merge(options)
-    send(options[:renderer], to_convert, options)
-  end
-
-  def self.html(html_body, options)
-    Parser.new(html_body).parse
-  end
-
-  def self.single(latex, options)
-    Render.new.render(latex)
+    send("render_#{options[:renderer]}", to_convert, options)
   end
 
   private
@@ -21,5 +13,13 @@ module Katex2HTML
     {
       renderer: :single
     }
+  end
+
+  def self.render_html(html_body, _options)
+    Parser.new(html_body).parse
+  end
+
+  def self.render_single(latex, _options)
+    Render.new.render(latex)
   end
 end
--- spec/katex2html_spec.rb
+++ spec/katex2html_spec.rb
@@ -1,11 +1,39 @@
 require 'spec_helper'
 
-describe Katex2html do
-  it 'has a version number' do
-    expect(Katex2html::VERSION).not_to be nil
-  end
+describe Katex2HTML do
+  describe '.render' do
+    it 'responds to render' do
+      expect(subject).to respond_to(:render)
+    end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+    context 'wihout options' do
+      it 'render with single renderer' do
+        expect(subject).to receive(:render_single)
+
+        Katex2HTML.render("mylatex")
+      end
+    end
+
+    context 'with options' do
+      describe ':html' do
+        it 'render with html renderer' do
+          expect(subject).to receive(:render_html)
+
+          Katex2HTML.render("mylatex", {renderer: :html})
+        end
+      end
+      describe ':single' do
+        it 'render with single renderer' do
+          expect(subject).to receive(:render_single)
+
+          Katex2HTML.render("mylatex", {renderer: :single})
+        end
+      end
+      describe 'invalid option' do
+        it 'raises MethodNotFound' do
+          expect { Katex2HTML.render("mylatex", renderer: :invalid) }.to raise_error(NoMethodError)
+        end
+      end
+    end
   end
 end
