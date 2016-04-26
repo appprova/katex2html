9adc696d7c408b05f074b32e4e22b81052573c23
Vasiliy Chernykh <shinkupro@gmail.com>
2016-04-26 21:37:33 +0500

    Add katex.js options to Renderer (#8)
    This allows Renderer to receive Katex.js options and pass through ExecJS to Katex renderer.

--- lib/katex2html/renderer.rb
+++ lib/katex2html/renderer.rb
@@ -2,11 +2,12 @@ require "execjs"
 
 module Katex2HTML
   class Renderer
-    def initialize(_options = {})
+    def initialize(options = {})
+      @options = default_opts.merge(options)
     end
 
     def render(latex)
-      context.call("katex.renderToString", latex)
+      context.call("katex.renderToString", latex, @options)
     end
 
     private
@@ -17,6 +18,14 @@ module Katex2HTML
 
     def source
       @source ||= File.read(File.expand_path("../../../vendor/katex.min.js", __FILE__))
+    end
+
+    def default_opts
+      {
+        displayMode: false,
+        throwOnError: true,
+        errorColor: '#cc0000'
+      }
     end
   end
 end
--- spec/katex2html/renderer_spec.rb
+++ spec/katex2html/renderer_spec.rb
@@ -2,11 +2,15 @@ require 'spec_helper'
 
 describe Katex2HTML::Renderer do
   let(:valid_latex) { "log_{ab}" }
+  let(:unsupported_latex) { "\\unsupportedcmd{ab}" }
   let(:invalid_latex) { "$log_{ab}$" }
-  let(:renderer) { Katex2HTML::Renderer.new }
+
+  def renderer(options = {})
+    Katex2HTML::Renderer.new(options)
+  end
 
   describe '#render' do
-    context 'with valid latex' do
+    context 'with valid supported latex' do
       it 'renders latex formule into Katex HTML' do
         rendered = renderer.render(valid_latex)
 
@@ -14,9 +18,28 @@ describe Katex2HTML::Renderer do
         expect(rendered).to include('katex')
       end
     end
+
+    context 'with valid unsupported latex' do
+      context "when renderer throwOnError option is true" do
+        it 'raises ExecJS::ProgramError' do
+          expect { renderer(throwOnError: true).render(unsupported_latex) }.to raise_error(ExecJS::ProgramError)
+        end
+      end
+
+      context "when renderer throwOnError option is false" do
+        it 'renders unsupported latex into Katex HTML' do
+          rendered = renderer(throwOnError: false).render(unsupported_latex)
+
+          expect(rendered).to_not eq(unsupported_latex)
+          expect(rendered).to include('katex')
+        end
+      end
+    end
+
     context 'with invalid latex' do
       it 'raises ExecJS::ProgramError' do
-        expect { renderer.render(invalid_latex) }.to raise_error(ExecJS::ProgramError)
+        expect { renderer(throwOnError: true).render(invalid_latex) }.to raise_error(ExecJS::ProgramError)
+        expect { renderer(throwOnError: false).render(invalid_latex) }.to raise_error(ExecJS::ProgramError)
       end
     end
 
